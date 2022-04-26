22c1581a5f067cf76c2d11189bbc742e80d48279
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-04-26 13:38:44 +0900

    操作追記

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float power = 10;
+    public Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidbody.AddForce(new Vector3(0, 0, 1) * power);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddForce(new Vector3(-1, 0, 0) * power);
+        }
     }
 }
