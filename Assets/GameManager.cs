8052bfb6883bf33829fa6a9ebb0ec16b838cc70d
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-05-17 15:27:03 +0900

    public付け忘れちゃ動かねーよ！（修正）

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,17 +8,17 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void SceneReset()
+    public void SceneReset()
     {
-        string activeSceneName=SceneManager.GetActiveScene().name;
+        string activeSceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(activeSceneName);
     }
 }
